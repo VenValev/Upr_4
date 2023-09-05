5385e44c4916affa2e43ba172ea52e05732782e6
Vencislav <vencislav67@hotmail.com>
2023-09-05 11:45:35 +0300

    Add project files.

---
+++ Upr_4/Upr_4.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <cstring>
+
+struct Book
+{
+    char title[30];
+    char author[30];
+    int ISBN;
+    int pageCount;
+    Book(const char* title, const char* author[9], int ISBN, int pageCount) : ISBN(ISBN), pageCount(pageCount + 5)
+    {
+
+    }
+};
+
+int main()
+{
+    
+
+}
+
+
